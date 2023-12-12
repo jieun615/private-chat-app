c2225534cc4572fe0066324cffd6cdfb0c504a67
jieun615 <je060105@gmail.com>
2023-12-12 14:31:44 +0900

    전역변수 추가

--- public/main.js
+++ public/main.js
@@ -5,3 +5,12 @@ const socket = io(process.env.SERVER_URL, {
 socket.onAny((event, ...args) => {
     console.log(event, ...args);
 })
+
+const chatBody = document.querySelector('.chat-body');
+const userTitle = document.querySelector('user-title');
+const loginContainer = document.querySelector('.login-container');
+const userTable = document.querySelector('.users');
+const userTagline = document.querySelector('#users-tagline');
+const title = document.querySelector('#active-user');
+const messages = document.querySelector('.messages');
+const msgDiv = document.querySelector('.msg-form');
