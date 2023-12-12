e5146edbeeb53801b2cbcf4dfe745c9a56e2934c
jieun615 <je060105@gmail.com>
2023-12-12 18:47:10 +0900

    유저 세션 데이터 생성

--- public/main.js
+++ public/main.js
@@ -7,7 +7,7 @@ socket.onAny((event, ...args) => {
 })
 
 const chatBody = document.querySelector('.chat-body');
-const userTitle = document.querySelector('user-title');
+const userTitle = document.querySelector('#user-title');
 const loginContainer = document.querySelector('.login-container');
 const userTable = document.querySelector('.users');
 const userTagline = document.querySelector('#users-tagline');
@@ -49,3 +49,25 @@ const socketConnect = async (username, userID) => {
 
     await socket.connect();
 }
+
+socket.on('users-data', ({ users }) => {
+    const index = users.findIndex(user => user.userID === socket.id);
+    if(index > -1) {
+        users.splice(index, 1);
+    }
+
+    let ul = `<table class="table table-hover">`;
+    for(const user of users) {
+        ul += `<tr class="socket-users" onclick="setActiveUser(this, '${user.username}', '${user.userID}')"><td>${user.username}<span class="text-danger ps-1 d-none" id=${user.userID}">!</span></td></tr>`;
+    }
+    ul += `</table>`;
+    if(users.length > 0) {
+        userTagline.innerHTML = '접속 중인 유저';
+        userTagline.classList.remove('text-danger');
+        userTagline.classList.add('text-success');
+    } else {
+        userTagline.innerHTML = '접속 중인 유저 없음';
+        userTagline.classList.remove('text-success');
+        userTagline.classList.add('text-danger');
+    }
+})
