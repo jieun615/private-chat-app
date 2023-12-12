aad5b51e4bc59dd6604ec71e20a09d15541051ec
jieun615 <je060105@gmail.com>
2023-12-12 12:30:38 +0900

    socker.io 연동

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const http = require('http');
+const { Server } = require('socket.io');
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 
 const port = process.env.SERVER_PORT;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`listening on ${port}`);
 });
